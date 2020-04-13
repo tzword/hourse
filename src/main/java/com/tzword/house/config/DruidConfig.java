295f8133b652ff2be32623e674e750c520af9d8b
shs-zhcsyfczyjhy <tzword@163.com>
2020-04-13 14:03:04 +0800

    druid连接池配置

---
+++ src/main/java/com/tzword/house/config/DruidConfig.java
@@ -0,0 +1,41 @@
+package com.tzword.house.config;
+
+import com.alibaba.druid.filter.Filter;
+import com.alibaba.druid.filter.stat.StatFilter;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.google.common.collect.Lists;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.ServletRegistration;
+
+@Configuration
+public class DruidConfig {
+    @ConfigurationProperties(prefix = "spring.druid")
+    @Bean(initMethod = "init",destroyMethod = "close")
+    public DruidDataSource dataSource(){
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setProxyFilters(Lists.newArrayList(statFilter()));
+        return druidDataSource;
+    }
+
+    @Bean
+    public Filter statFilter(){
+        StatFilter statFilter = new StatFilter();
+        statFilter.setSlowSqlMillis(1);
+        statFilter.setLogSlowSql(true);
+        statFilter.setMergeSql(true);
+        return statFilter;
+    }
+
+    /**
+     * 监控
+     */
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(){
+        return new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+    }
+}
