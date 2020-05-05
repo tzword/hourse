093f21e8a53fc984c6ff2c69e56216dbfea8da96
tzword <tzword@163.com>
2020-05-05 21:01:51 +0800

    用户注册校验，公用消息返回

--- house-web/src/main/java/com/tzword/house/web/controller/UserController.java
+++ house-web/src/main/java/com/tzword/house/web/controller/UserController.java
@@ -2,6 +2,7 @@ package com.tzword.house.web.controller;
 
 import com.tzword.house.common.model.User;
 import com.tzword.house.biz.service.UserService;
+import com.tzword.house.common.result.ResultMsg;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +20,11 @@ public class UserController {
     @RequestMapping("/getUser")
     public List<User> getUsers(){
         return userService.getUsers();
+    }
+
+    @RequestMapping("/register")
+    public String register(User user){
+        ResultMsg validate = UserHelper.validate(user);
+        return null;
     }
 }
